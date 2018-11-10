5fe8a3be8aefc58f5414bdfcab08a95dbde15c3f
Miquel <rossellomelismiquel@gmail.com>
2018-11-10 18:16:01 +0100

    Proves JSON

--- JSON/creacio_json/json_creator.php
+++ JSON/creacio_json/json_creator.php
@@ -1,9 +1,12 @@
 <?php
-    header("Content-Type: application/json; charset=UTF-8");
+    /*header("Content-Type: application/json; charset=UTF-8");
     $conn = new mysqli("192.168.1.20", "root", "mrm1998", "act_json_1");
     $statement = $conn->prepare("SELECT * FROM amarrament");
     $statement->execute();
     $result = $statement->get_result();
-    $output = $result->fetch_all(MYSQL_ASSOC);
+    $output = $result->fetch_all(MYSQL_ASSOC);*/
+    $array = array("Miquel", "Tomeu");
+
+    $output = json_encode($array);
 
     echo json_encode($output);
